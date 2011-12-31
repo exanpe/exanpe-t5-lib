98acc81d3c25a320ddd469892888c87ebd78f169
Laurent Guerin <laurent@dreamcat.fr>
2011-12-31 15:58:32 +0100

    #10 Filter Rich Text Editor toolbar buttons with include / exclude parameters

--- src/main/resources/fr/exanpe/t5/lib/components/js/exanpe-t5-lib.js
+++ src/main/resources/fr/exanpe/t5/lib/components/js/exanpe-t5-lib.js
@@ -3373,9 +3373,11 @@ Tapestry.Initializer.listSorterBuilder = function(data){
  * @param {String} id the id of the RichTextEditor Mixin container element
  * @param {String} title the title of the dialog box
  * @param {boolean} collapse indicating if the the toolbar should have a collapse button or not
+ * @param {Array} include list of buttons to include : only theses buttons will be retained
+ * @param {Array} exclude list of buttons to exclude
  * @param {Object.<string, string>} messages the messages used to display Toolbar buttons labels 
  */
-Exanpe.RichTextEditor = function(id, title, width, height, autofocus, collapse, messages) {
+Exanpe.RichTextEditor = function(id, title, width, height, autofocus, collapse, include, exclude, messages) {
 	/**
 	 * The id of the instance
 	 */
@@ -3397,6 +3399,16 @@ Exanpe.RichTextEditor = function(id, title, width, height, autofocus, collapse, 
 	this.height = height;
 	
 	/**
+	 * List of buttons to include (and only these buttons)
+	 */
+	this.include = include;
+	
+	/**
+	 * List of buttons to exclude
+	 */
+	this.exclude = exclude;
+	
+	/**
 	 * Autofocus the textarea on display or not
 	 */
 	this.autofocus = autofocus;
@@ -3409,7 +3421,12 @@ Exanpe.RichTextEditor = function(id, title, width, height, autofocus, collapse, 
 	/**
 	 * YUI Editor config object
 	 */
-	this.config = null
+	this.config = null;
+	
+	/**
+	 * List of available buttons
+	 */
+	this.buttons = {};
 	
 	/**
 	 * The wrapped YUI Editor widget
@@ -3423,7 +3440,7 @@ Exanpe.RichTextEditor = function(id, title, width, height, autofocus, collapse, 
 };
 
 /**
- * Get the toolbar object of the RichTextEditor
+ * Get the YUI toolbar object of the RichTextEditor, ONCE loaded.
  * @returns {Object} the json object used to configure the toolbar
  */
 Exanpe.RichTextEditor.prototype.getToolbar = function() {
@@ -3439,6 +3456,40 @@ Exanpe.RichTextEditor.prototype.getConfig = function() {
 };
 
 /**
+ * Load the list of availables buttons for Rich Text Editor
+ * @private
+ */
+Exanpe.RichTextEditor.prototype._loadAvailableButtons = function() {
+	this.buttons['separator'] = { type: 'separator' };
+	this.buttons['bold'] = { type: 'push', label: this.messages.bold, value: 'bold' }; 
+	this.buttons['italic'] = { type: 'push', label: this.messages.italic, value: 'italic' };
+	this.buttons['underline'] = { type: 'push', label: this.messages.underline, value: 'underline' }; 
+	this.buttons['justifyleft'] = { type: 'push', label: this.messages.justifyleft, value: 'justifyleft' };
+	this.buttons['justifycenter'] = { type: 'push', label: this.messages.justifycenter, value: 'justifycenter' };
+	this.buttons['justifyright'] = { type: 'push', label: this.messages.justifyright, value: 'justifyright' };
+	this.buttons['createlink'] = { type: 'push', label: this.messages.createlink, value: 'createlink', disabled: true };
+	this.buttons['insertunorderedlist'] = { type: 'push', label: this.messages.insertunorderedlist, value: 'insertunorderedlist' };
+	this.buttons['insertorderedlist'] = { type: 'push', label: this.messages.insertorderedlist, value: 'insertorderedlist' };
+	this.buttons['undo'] = { type: 'push', label: this.messages.undo, value: 'undo', disabled: true };
+	this.buttons['redo'] = { type: 'push', label: this.messages.redo, value: 'redo', disabled: true };
+	this.buttons['heading'] =  { type: 'select', label: this.messages.heading, value: 'heading', disabled: true,
+					            menu: [
+					                   { text: this.messages.none, value: 'none', checked: true },
+					                   { text: this.messages.h1, value: 'h1' },
+					                   { text: this.messages.h2, value: 'h2' },
+					                   { text: this.messages.h3, value: 'h3' },
+					                   { text: this.messages.h4, value: 'h4' },
+					                   { text: this.messages.h5, value: 'h5' },
+					                   { text: this.messages.h6, value: 'h6' }
+					               ]
+					           };
+	this.buttons['fontsize'] = { type: 'spin', label: '13', value: 'fontsize', range: [ 8, 72 ], disabled: true };
+	this.buttons['forecolor'] = { type: 'color', label: this.messages.forecolor, value: 'forecolor', disabled: true };
+	this.buttons['backcolor'] = { type: 'color', label: this.messages.backcolor, value: 'backcolor', disabled: true };
+};
+
+
+/**
  * Return the container of the editor
  * @return {HTMLElement} the DOM element corresponding to the editor container
  */
@@ -3450,7 +3501,7 @@ Exanpe.RichTextEditor.prototype.getContainer = function() {
  * Configure the default toolbar of the YUI Editor
  * @private
  */
-Exanpe.RichTextEditor.prototype._configToolbar = function() {
+Exanpe.RichTextEditor.prototype._configDefaultToolbar = function() {
 	this.config = {
             height: this.height + 'px',
             width: this.width + 'px',
@@ -3462,50 +3513,40 @@ Exanpe.RichTextEditor.prototype._configToolbar = function() {
                 buttons: [
 	                { group: 'alignment',
 		                      buttons: [
-		                          { type: 'push', label: this.messages.bold, value: 'bold' },
-		                          { type: 'push', label: this.messages.italic, value: 'italic' },
-		                          { type: 'push', label: this.messages.underline, value: 'underline' },
-		                          { type: 'separator' },                                  
-		                          { type: 'push', label: this.messages.justifyleft, value: 'justifyleft' },
-		                          { type: 'push', label: this.messages.justifycenter, value: 'justifycenter' },
-		                          { type: 'push', label: this.messages.justifyright, value: 'justifyright' },
-		                          { type: 'separator' },                                  
-		                          { type: 'push', label: this.messages.createlink, value: 'createlink', disabled: true }
+		                          this.buttons['bold'],
+		                          this.buttons['italic'],
+		                          this.buttons['underline'],
+		                          this.buttons['separator'],                                  
+		                          this.buttons['justifyleft'],
+		                          this.buttons['justifycenter'],
+		                          this.buttons['justifyright'],
+		                          this.buttons['separator'],                                  
+		                          this.buttons['createlink']
 		                      ]
 	                },                          
 	                { group: 'indentlist',
 	                    buttons: [
-	                        { type: 'push', label: this.messages.insertunorderedlist, value: 'insertunorderedlist' },
-	                        { type: 'push', label: this.messages.insertorderedlist, value: 'insertorderedlist' }
+	                        this.buttons['insertunorderedlist'],
+	                        this.buttons['insertorderedlist']
 	                    ]
 	                },               
                     { group: 'undoredo',
                     		buttons: [
-			                          { type: 'push', label: this.messages.undo, value: 'undo', disabled: true },
-			                          { type: 'push', label: this.messages.redo, value: 'redo', disabled: true }
+			                          this.buttons['undo'],
+			                          this.buttons['redo']
 			                ]
                     },
                     { group: 'parastyle',
                         buttons: [
-                        { type: 'select', label: this.messages.heading, value: 'heading', disabled: true,
-                            menu: [
-                                { text: this.messages.none, value: 'none', checked: true },
-                                { text: this.messages.h1, value: 'h1' },
-                                { text: this.messages.h2, value: 'h2' },
-                                { text: this.messages.h3, value: 'h3' },
-                                { text: this.messages.h4, value: 'h4' },
-                                { text: this.messages.h5, value: 'h5' },
-                                { text: this.messages.h6, value: 'h6' }
-                            ]
-                        }
+                                  	  this.buttons['heading']
                         ]
                     },
                     { group: 'textstyle',
                         buttons: [
-                            { type: 'spin', label: '13', value: 'fontsize', range: [ 8, 72 ], disabled: true },
-                            { type: 'separator' },
-                            { type: 'color', label: this.messages.forecolor, value: 'forecolor', disabled: true },
-                            { type: 'color', label: this.messages.backcolor, value: 'backcolor', disabled: true }
+                            this.buttons['fontsize'],
+                            this.buttons['separator'],
+                            this.buttons['forecolor'],
+                            this.buttons['backcolor']
                         ]
                     }
                 ]
@@ -3533,6 +3574,52 @@ Exanpe.RichTextEditor.prototype.save = function() {
 	this.getParentForm().on(Tapestry.FORM_PREPARE_FOR_SUBMIT_EVENT, function() {yui.saveHTML();});
 };
 
+
+/**
+ * Build the Toolbar with buttons choosed by user within "include" parameter.
+ * Only these buttons will be retained.
+ * @private
+ */
+Exanpe.RichTextEditor.prototype._includeToolbarButtons = function() {
+	var len = this.include.length;
+	if (len > 0) {
+		this.config['toolbar']['buttons'] = [];
+		var custom = {};
+		custom['group'] = 'user_include';
+		custom['buttons'] = [];
+		for (var i=0; i < len; i++) {
+			var customButton = this.include[i];
+			if (this.buttons[customButton]) {
+				custom['buttons'].push(this.buttons[customButton]);
+			}
+			else {
+				Exanpe.Log.error("Button: '" + customButton + "' does not exist for Exanpe Rich Text Editor.");
+			}
+		}
+		this.config['toolbar']['buttons'].push(custom);
+	}
+};
+
+/**
+ * Exclude the buttons contained into "exclude" parameter from the default toolbar
+ * @private
+ */
+Exanpe.RichTextEditor.prototype._excludeToolbarButtons = function() {
+	var groups = this.config['toolbar']['buttons'];
+	var i, j;
+	for(i=0; i<groups.length; i++) {
+		var group = this.config['toolbar']['buttons'][i];
+		var buttons = group['buttons'];
+		var len = buttons.length;
+		for (j=0; j < len; j++) {
+			var button = this.config['toolbar']['buttons'][i]['buttons'][j];
+			if (this.exclude.indexOf(button.value) >= 0) {
+				this.config['toolbar']['buttons'][i]['buttons'][j] = '';
+			}
+		}
+	}
+};
+
 /**
  * Called before rendering RichTextEditor component
  * Does nothing by default, override to define your own action.
@@ -3546,10 +3633,19 @@ Exanpe.RichTextEditor.prototype.beforeRenderRichTextEditor = function() {
  * @private
  */
 Exanpe.RichTextEditor.prototype._init = function(){
-	// Toolbar config
-	this._configToolbar();
+	// Load available Toolbar buttons
+	this._loadAvailableButtons();
 	
-	// Init editor and replace the HTML textarea
+	// Toolbar config
+	this._configDefaultToolbar();
+	
+	// Include buttons only
+	this._includeToolbarButtons();
+	
+	// Exclude buttons
+	this._excludeToolbarButtons();
+	
+	// Init editor
 	this.yui = new YAHOO.widget.Editor(this.id, this.config);
 	this.yui.wrapper = this;
 
@@ -3558,7 +3654,7 @@ Exanpe.RichTextEditor.prototype._init = function(){
 		this.wrapper.beforeRenderRichTextEditor();
 	}, this.yui, true);
 
-	// Render editor
+	// Render editor and replace the HTML textarea
 	this.yui.render();
 	
 	// Save html content on submit event
@@ -3572,7 +3668,7 @@ Exanpe.RichTextEditor.prototype._init = function(){
  * @static
  */
 Tapestry.Initializer.richTextEditorBuilder = function(data){
-	var rte = new Exanpe.RichTextEditor(data.id, data.title, data.width, data.height, data.autofocus, data.collapse===true, YAHOO.lang.JSON.parse(data.messages));
+	var rte = new Exanpe.RichTextEditor(data.id, data.title, data.width, data.height, data.autofocus, data.collapse===true, YAHOO.lang.JSON.parse(data.include), YAHOO.lang.JSON.parse(data.exclude), YAHOO.lang.JSON.parse(data.messages));
 	rte._init();
 	window[data.id] = rte;
 };
